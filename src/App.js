0013e2c90c9cebd9ba5f4f764b410944ced0431e
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-06-04 13:09:11 +0200

    feat: use new containers and scrollbars

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from "react";
 import Tabs from "./components/Tabs";
 import Tab from "./components/Tab";
 
+import Landing from "./containers/Landing";
+import Bitcoin from "./containers/Bitcoin";
+import Pokemon from "./containers/Pokemon";
+import ScrollBars from "./components/ScrollBars";
+
 export function App() {
   return (
-    <div className="container">
-      <div className="nes-container is-dark">
-        <h1 className="nes-text is-disabled">Parcel App</h1>
+    <ScrollBars>
+      <div className="container">
+        <div className="nes-container is-dark">
+          <h1 className="nes-text is-disabled with-work-break">Parcel App</h1>
+        </div>
+        <Tabs>
+          <Tab order={0} label="1st" title="First Tab" importance="is-primary">
+            <Landing />
+          </Tab>
+          <Tab order={1} label="2nd" title="Second Tab" importance="is-success">
+            <Bitcoin />
+          </Tab>
+          <Tab order={2} label="3rd" title="Third Tab" importance="is-error">
+            <Pokemon />
+          </Tab>
+        </Tabs>
       </div>
-      <Tabs>
-        <Tab order={0} label="1st" title="First Tab" importance="is-primary">
-          <p>First</p>
-        </Tab>
-        <Tab order={1} label="2nd" title="Second Tab" importance="is-success">
-          <p>Second</p>
-        </Tab>
-        <Tab order={2} label="3rd" title="Third Tab" importance="is-error">
-          <p>Third</p>
-        </Tab>
-      </Tabs>
-    </div>
+    </ScrollBars>
   );
 }
 
