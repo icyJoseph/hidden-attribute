41331efa733fe21fe73facf5d9f3a03456d58b7a
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-11-24 06:08:17 +0100

    feat: scripts to aggregate data

---
+++ genBars.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const all = require("./stats/all.json");
+const bitcoin = require("./stats/bitcoin.json");
+const display = require("./stats/display.json");
+const hidden = require("./stats/hidden.json");
+const landing = require("./stats/landing.json");
+const visibility = require("./stats/visibility.json");
+
+const bars = [all, bitcoin, display, hidden, landing, visibility]
+  .reduce((prev, { label, averages }) => {
+    const values = averages.reduce(
+      (acc, { avg, groupLabel }) => {
+        acc[groupLabel] = Math.floor(avg);
+        acc.avg = acc.avg + avg;
+        return acc;
+      },
+      { avg: 0 }
+    );
+    return prev.concat({ label, ...values });
+  }, [])
+  .sort((a, b) => a.avg - b.avg)
+  .map(({ avg, ...rest }) => ({ ...rest }));
+
+fs.writeFile(
+  path.join(__dirname, "./presentation/bars.json"),
+  JSON.stringify(bars),
+  err => {
+    if (err) return console.log(err);
+    return console.log("Successfully saved data as bars");
+  }
+);
---
+++ measure.js
@@ -0,0 +1,102 @@
+const { spawn } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const AUDITS = "audits";
+const WORK = "mainthread-work-breakdown";
+
+const avg = arr =>
+  arr.length ? arr.reduce((acc, curr) => acc + curr, 0) / arr.length : 0;
+
+const repeats = Array.from({ length: 20 }, (_, i) => i);
+
+// slow on purpose to avoid spamming the used API's
+fs.readdir(path.join(__dirname, "./results"), async (_, dirs) => {
+  for (const dir of dirs) {
+    console.log(`Checking out: ${dir}`);
+    const checkout = spawn(`git checkout ${dir}`, { shell: true });
+    await new Promise(resolve => {
+      checkout.on("close", resolve);
+    });
+    let iteration;
+    for (const repeat of repeats) {
+      console.log(`Auditing: ${dir}`);
+      const measure = spawn("yarn build:audit", {
+        shell: true
+      });
+
+      await new Promise(resolve => {
+        measure.on("close", resolve);
+      });
+      iteration = repeat;
+    }
+
+    console.log(`Done auditing ${dir}, ${iteration + 1} times`);
+
+    let results = [];
+    const all = await new Promise(accept => {
+      fs.readdir(path.join(__dirname, "./results", dir), async (_, files) => {
+        for (const file of files) {
+          const data = await new Promise(resolve => {
+            return fs.readFile(
+              path.join(__dirname, "./results", dir, file),
+              (err, data) => {
+                if (err) return console.log(err);
+                return resolve(JSON.parse(data));
+              }
+            );
+          });
+          results.push(data);
+        }
+        accept(results);
+      });
+    });
+
+    const aggregate = all
+      .filter(
+        ({
+          [AUDITS]: {
+            [WORK]: { details }
+          }
+        }) => !!details
+      )
+      .reduce(
+        (
+          prev,
+          {
+            [AUDITS]: {
+              [WORK]: {
+                details: { items }
+              }
+            }
+          }
+        ) => prev.concat(items),
+        []
+      )
+      .reduce((acc, { group, duration, ...rest }) => {
+        const data = acc[group];
+        if (data) {
+          return {
+            ...acc,
+            [group]: { ...data, duration: data.duration.concat(duration) }
+          };
+        }
+        return { ...acc, [group]: { group, duration: [duration], ...rest } };
+      }, {});
+
+    const averages = Object.values(aggregate).map(({ duration, ...rest }) => ({
+      ...rest,
+      duration,
+      avg: avg(duration)
+    }));
+
+    fs.writeFile(
+      path.join(__dirname, "./stats", `${dir}.json`),
+      JSON.stringify({ label: dir, averages }),
+      err => {
+        if (err) return console.log(err);
+        return console.log("Done saving stats for: ", dir);
+      }
+    );
+  }
+});
