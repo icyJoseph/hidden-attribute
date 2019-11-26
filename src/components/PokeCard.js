49f1a87e6d63731ae427f61604fa9c701e761fdb
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-11-26 18:13:54 +0100

    feat: suspense list

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { idFromUrl, spriteUrl } from "../utils";
+import { wrapPromise } from "../utils";
 
-export default ({ name, url, showImg }) => {
-  const imgSrc = spriteUrl(idFromUrl(url));
+const delay = (res, time) =>
+  new Promise(accept => setTimeout(() => accept(res), time));
+
+export const fetchImage = (url, index) =>
+  wrapPromise(
+    fetch(url)
+      .then(res => delay(res, index * 1000))
+      .then(response => response.blob())
+      .then(data => {
+        const objectURL = URL.createObjectURL(data);
+        return objectURL;
+      })
+  );
+
+export const Image = ({ resource }) => (
+  <img className="pokemon-avatar" src={resource.read()} alt="🤔" />
+);
+
+export default ({ name, resource }) => {
+  const deferredResource = React.useDeferredValue(resource, {
+    timeoutMs: 5000
+  });
 
   return (
     <div
       key={name}
       className="nes-container with-title is-dark with-background"
-      style={{
-        backgroundImage: !showImg && `url(${imgSrc})`
-      }}
     >
       <p className="title">{name}</p>
-      {showImg && <img className="pokemon-avatar" src={imgSrc} alt="🤔" />}
+      <React.Suspense fallback={<i className="nes-pokeball moving" />}>
+        <Image resource={deferredResource} />
+      </React.Suspense>
     </div>
   );
 };
